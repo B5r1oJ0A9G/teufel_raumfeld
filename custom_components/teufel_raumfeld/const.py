ca14d900b7f94213bd692c4c6665aaa0ad793ad7
Marc <nobody@example.com>
2021-01-31 19:59:40 +0100

    sorting constants

--- custom_components/teufel_raumfeld/const.py
+++ custom_components/teufel_raumfeld/const.py
@@ -1,20 +1,17 @@
 """Constants for the Teufel Raumfeld integration."""
 CHANGE_STEP_VOLUME_DOWN = -2
 CHANGE_STEP_VOLUME_UP = 5
+DEFAULT_HOST_WEBSERVICE = "raumfeld-host.example.com"
+DEFAULT_PORT_WEBSERVICE = "47365"
 DEVICE_CLASS_SPEAKER = "speaker"
 DEVICE_MANUFACTURER = "Teufel Audio GmbH"
 DOMAIN = "teufel_raumfeld"
-DEFAULT_HOST_WEBSERVICE = "raumfeld-host.example.com"
-DEFAULT_PORT_WEBSERVICE = "47365"
 GROUP_PREFIX = "Group: "
+MEDIA_CONTENT_ID_SEP = "[:sep:]"
 PLATFORMS = ["media_player"]
 ROOM_PREFIX = "Room: "
-SERVICE_SNAPSHOT = "snapshot"
 SERVICE_RESTORE = "restore"
-UPNP_CLASS_ALBUM = "object.container.album.musicAlbum"
-UPNP_CLASS_TRACK = "object.item.audioItem.musicTrack"
-URN_CONTENT_DIRECTORY = "urn:upnp-org:serviceId:ContentDirectory"
-MEDIA_CONTENT_ID_SEP = "[:sep:]"
+SERVICE_SNAPSHOT = "snapshot"
 SUPPORTED_OBJECT_IDS = ["0", "0/DemoTracks", "0/My Music"]
 SUPPORTED_OBJECT_PREFIXES = [
     "0/My Music/Artists",
@@ -26,3 +23,6 @@ SUPPORTED_OBJECT_PREFIXES = [
     "0/My Music/AllTracks",
     "0/DemoTracks",
 ]
+UPNP_CLASS_ALBUM = "object.container.album.musicAlbum"
+UPNP_CLASS_TRACK = "object.item.audioItem.musicTrack"
+URN_CONTENT_DIRECTORY = "urn:upnp-org:serviceId:ContentDirectory"
